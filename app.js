5d9b4c99663a35b4ecae9bdeb28d6e98f8faa1d7
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-02-11 00:39:26 +0700

    update and fix bug

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
   $(".slider").slick({
-    arrows: false,
     dots: false,
     infinite: false,
-    initialSlide: 0,
-    slidesToScroll: 0,
+    speed: 0,
     slidesToShow: 4,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
+          infinite: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
       {
@@ -19,7 +26,6 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          dots: true,
         },
       },
     ],
@@ -47,13 +53,12 @@ $(document).ready(function () {
     dots: false,
     infinite: false,
     initialSlide: 0,
-    slidesToScroll: 0,
     slidesToShow: 8,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 769,
         settings: {
-          slidesToShow: 6.5,
+          slidesToShow: 5,
           slidesToScroll: 1,
         },
       },
@@ -72,21 +77,20 @@ $(document).ready(function () {
     dots: false,
     infinite: false,
     initialSlide: 0,
-    slidesToScroll: 0,
     slidesToShow: 6,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 769,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           dots: true,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1.8,
+          slidesToShow: 1.5,
           slidesToScroll: 2,
           dots: true,
         },
@@ -94,3 +98,9 @@ $(document).ready(function () {
     ],
   });
 });
+const hamburger = document.querySelector(".hamburger");
+const menuNav = document.querySelector(".nav_list_item");
+
+hamburger.onclick = function () {
+  menuNav.classList.toggle("active");
+};
